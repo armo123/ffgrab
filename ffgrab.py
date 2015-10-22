3489219869c2595bde952861a53df9d12b5412c5
armo <armo.arma23@gmail.com>
2015-10-22 15:27:17 +0100

    General enhacements and gui tweaking
    some icons for the record and stop buttons added
    audio codec argument added
    comments corrected
    audio and video device selection in the next commit
    tray icon required

--- ffgrab.py
+++ ffgrab.py
@@ -6,11 +6,12 @@
 # Module: ffgrab     #
 ######################
 
-# Description: this is a simple gui wraper
+# Description: this is a simple gui wrapper
 # for ffmpeg to record desktop
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtGui import QApplication, QWidget, QLabel, QMainWindow, QComboBox
+from PyQt4.QtGui import QPushButton, QHBoxLayout, QVBoxLayout,\
+    QCheckBox, QIcon, QSpinBox
 import sys
 from subprocess import Popen
 
@@ -23,13 +24,18 @@ class mainW(QMainWindow):
         self.labelRes = QLabel('Resolution')
         self.comboRes = QComboBox()
         self.buttonRec = QPushButton("Record")
+        self.buttonRec.setIcon(QIcon("icons/player_record.png"))
         self.buttonStop = QPushButton("Stop")
+        self.buttonStop.setIcon(QIcon("icons/player_stop.png"))
+        self.recordLayout = QHBoxLayout()
+        self.recordLayout.addWidget(self.buttonRec)
+        self.recordLayout.addWidget(self.buttonStop)
+        # retrieve  available resolutions on the system
         self.sh = ['sh', 'resgrab.sh']
         Popen(self.sh)
         with open("res.txt", "r") as res:
             for line in res:
                 self.comboRes.addItem(line)
-
         self.resLayout = QHBoxLayout()
         self.resLayout.addWidget(self.labelRes)
         self.resLayout.addWidget(self.comboRes)
@@ -45,36 +51,38 @@ class mainW(QMainWindow):
         self.layoutAudio.addWidget(self.labelAudio)
         self.layoutAudio.addWidget(self.checkAudio)
         self.centralLayout = QVBoxLayout()
-        self.centralLayout.addWidget(self.buttonRec)
-        self.centralLayout.addWidget(self.buttonStop)
+        self.centralLayout.addLayout(self.recordLayout)
         self.centralLayout.addLayout(self.resLayout)
         self.centralLayout.addLayout(self.layoutRate)
-        self.centralLayout.addWidget(self.audio)
+        self.centralLayout.addLayout(self.layoutAudio)
         self.centralWidget.setLayout(self.centralLayout)
         self.setCentralWidget(self.centralWidget)
 
         def record():
             res = str(self.comboRes.currentText()).rstrip('\n')
             rate = str(self.spinRate.value())
-            grabAudio = [
+            grabAudioVideo = [
                 'ffmpeg', '-f',
                 'x11grab', '-r',
                 rate, '-s',
                 res, '-i',
                 ':0.0', '-f',
-                'pulse',
+                'pulse', '-ac', '2',
                 '-i', 'default',
-                '/home/armo/capture/output.mkv']
-            if self.audio.isChecked():
-                Popen(grabAudio)
+                'capture/output.mkv']
+            if self.checkAudio.isChecked():
+                Popen(grabAudioVideo)
+                print(grabAudioVideo)
             else:
-                # Remove unessary items to grab the video only
-                grabAudio.pop(12)
-                grabAudio.pop(11)
-                grabAudio.pop(10)
-                grabAudio.pop(9)
-                defaultRecord = grabAudio
-                Popen(defaultRecord)
+                # Remove unnecessary items to grab the video only
+                grabAudioVideo.pop(14)
+                grabAudioVideo.pop(13)
+                grabAudioVideo.pop(12)
+                grabAudioVideo.pop(11)
+                grabAudioVideo.pop(10)
+                grabAudioVideo.pop(9)
+                grabVideo = grabAudioVideo
+                Popen(grabVideo)
 
         def stopRecord():
             kill = ['pkill', '-15', 'ffmpeg']
